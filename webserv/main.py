f9a3a04cc0d052dcb1813ecd872f36b4cc042798
Zachary Jones <zj35635@gmail.com>
2025-02-17 19:24:50 -0600

    Added zip file creation and pyzipper to requirements

--- webserv/main.py
+++ webserv/main.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, send_file #using flask for webserver
 from pptx import Presentation
-import os
+import pyzipper, os
 
 app = Flask(__name__) #creates flask application
 pres = Presentation()
 inputs = []
-saveName = 'test.pptx'
+presName = 'test.pptx'
 files = [5]
+directoryName = "scenario.zip"
 
 @app.route('/submission', methods=['POST']) #user clicks submit on any of the questions
 def submission():
@@ -28,16 +29,29 @@ def complete():
     title.text = "Cybersecurity Scenario!"
     subtitle.text = "for " + inputs[0] + "'s class" #adds the user input into the presentation
     try:
-        pres.save(saveName)
+        pres.save(presName)
         print("Saved successfully")
     except:
         return "Error saving presentation", 500
     
+    with pyzipper.AESZipFile("archive.zip", 'w', compression=pyzipper.ZIP_LZMA, encryption=pyzipper.WZ_AES) as zipf:
+        zipf.setpassword(inputs[0].encode("utf-8"))
+        with open("test.txt", 'w') as file:
+            file.write("We did it!\n")
+        zipf.write("test.txt")
+        print("Password set")
+
+    with pyzipper.ZipFile(directoryName, 'w', compression=pyzipper.ZIP_LZMA) as zipf:
+        zipf.write("archive.zip")
+        zipf.write(presName)
+        os.remove("test.txt")
+        os.remove("archive.zip")
+        os.remove(presName)
+
     inputs.clear()
 
     try:
-        path = "../" + saveName
-        return send_file(path, mimetype='pptx', as_attachment=True, download_name=saveName)
+        return send_file("../" + directoryName, mimetype='zip', as_attachment=True, download_name=directoryName)
     except:
         return "Error uploading file. Please try again later."
 
